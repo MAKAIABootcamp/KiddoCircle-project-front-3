1acd85da5192bd5e58df7ca19858bf9704a70e75
dpaeza <danipaez1098@gmail.com>
2023-04-14 20:08:58 -0500

    create react app

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+
+    return (
+        <section >
+            
+        </section>
+    );
+};
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RouterDom from './router/routes';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  
+  <RouterDom/>
+  
+);
+
+
