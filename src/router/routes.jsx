e1e511dd931677ccf4635ad53ec984e3d783d973
glgaviria64 <glgaviria@gmail.com>
2023-04-17 09:12:16 -0500

    rename icons

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../components/Home/Home";
+import Profile from "../components/Profile/Profile"
 
 const RouterDom = () => { 
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/profile" element={<Profile />} />
             </Routes>
         </BrowserRouter>
     );
