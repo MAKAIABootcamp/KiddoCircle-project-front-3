a442bed031c03f8d8fc76ee840bf0d73d8d7b8a7
unknown <quiensoyanna99@gmail.com>
2023-04-17 16:36:24 -0500

    Add images and products carousels

---
+++ src/components/Home/buttons-section/ButtonsSection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ButtonsSection = () => {
+  return (
+    <section className="">
+      <article>
+        <img />
+        <h1>Ropa</h1>
+      </article>
+    </section>
+  );
+};
+
+export default ButtonsSection;
---
+++ src/components/Home/gradient-division/GradientDivision.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./gradientDivision.scss";
+
+const GradientDivision = () => {
+  return (
+    <div className="gradient-division">
+      {/* <p className="gradient-division__text">
+        ¡Únete a nuestra comunidad y comienza a comprar y vender artículos
+        infantiles usados hoy mismo! Además de encontrar buenos precios y
+        artículos en buen estado, podrás conocer a otras familias y hacer nuevos
+        amigos en el proceso. ¡Gracias por ser parte de nuestra comunidad!
+      </p> */}
+    </div>
+  );
+};
+
+export default GradientDivision;
