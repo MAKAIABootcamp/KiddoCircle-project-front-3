886a7c310388b8cd96e7e08bc17b651ab81bcc37
dpaeza <danipaez1098@gmail.com>
2023-04-17 15:05:02 -0500

    add toys layout

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../components/Home/Home";
 import Clothes from "../components/Clothes/Clothes";
-import NavBar from "../components/Navbar/NavBar"
+import NavBar from "../components/Navbar/NavBar";
+import Toys from "../components/Toys/Toys";
 
 const RouterDom = () => { 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/"  element={<NavBar />} >
+                <Route path="/" element={<NavBar />}>
                     <Route path="/" element={<Home />} />
                     <Route path="/ropa" element={<Clothes />} />
-                    <Route path="/juguetes" element="" />
+                    <Route path="/juguetes" element={<Toys />} />
                     <Route path="/articulos" element="" />
                     <Route path="/mi-cuenta" element="" />
                 </Route>
