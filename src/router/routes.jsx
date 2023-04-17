3a73cdb95173cb4f77804ddcc5648c70e70249fe
dpaeza <danipaez1098@gmail.com>
2023-04-16 23:58:57 -0500

    add ropa layout

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../components/Home/Home";
+import Clothes from "../components/Clothes/Clothes";
 
 const RouterDom = () => { 
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/ropa" element={<Clothes />} />
             </Routes>
         </BrowserRouter>
     );
