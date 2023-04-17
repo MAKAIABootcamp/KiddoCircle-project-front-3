0d0515be1fc038b1786dd5b6559402f07e53dd0c
JuranyR <adelerapi95@gmail.com>
2023-04-17 10:18:41 -0500

    add navbar

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../components/Home/Home";
 import Clothes from "../components/Clothes/Clothes";
+import NavBar from "../components/Navbar/NavBar"
 
 const RouterDom = () => { 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/ropa" element={<Clothes />} />
+                <Route path="/"  element={<NavBar />} >
+                    <Route path="/" element={<Home />} />
+                    <Route path="/ropa" element={<Clothes />} />
+                    <Route path="/juguetes" element="" />
+                    <Route path="/articulos" element="" />
+                    <Route path="/mi-cuenta" element="" />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
