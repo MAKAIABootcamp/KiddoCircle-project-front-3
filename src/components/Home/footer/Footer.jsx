d70dcefd11a4f563a37ce74c44ea8c82ccaa9439
dpaeza <danipaez1098@gmail.com>
2023-05-04 18:24:01 -0500

    change footer at home component

--- src/components/Home/footer/Footer.jsx
+++ src/components/Home/footer/Footer.jsx
@@ -3,14 +3,13 @@ import "./footer.scss";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <p className="footer__text">
-        Made with 💗 by <a href="https://github.com/dpaeza">dpaeza</a> ,{" "}
-        <a href="https://github.com/JuranyR">JuranyR</a>,{" "}
-        <a href="https://github.com/AnaM8129">AnaM8129</a> and
-        <a href="https://github.com/glgaviria64"> glgaviria64</a>
-      </p>
-    </div>
+      <div className="footer">
+          <p className="footer__text">
+              Made with 💗 by <a href="https://github.com/dpaeza">dpaeza</a> ,{" "}
+              <a href="https://github.com/JuranyR">JuranyR</a> and{" "}
+              <a href="https://github.com/AnaM8129">AnaM8129</a>
+          </p>
+      </div>
   );
 };
 
